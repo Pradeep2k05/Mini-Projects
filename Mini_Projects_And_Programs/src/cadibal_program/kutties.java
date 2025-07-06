6f1499668af719ed3bc7cb5e079dd9811296dff6
java program <mudunjakandupudi006@gmail.com>
2025-07-06 12:42:57 +0530

    examples

---
+++ Mini_Projects_And_Programs/src/cadibal_program/kutties.java
@@ -0,0 +1,69 @@
+package cadibal_program;
+import java.util.*;
+public class kutties {
+
+	public static void main(String[]args) {
+		
+		
+		//System.out.println("hello world");
+		
+		
+	/*	int a =5;
+		int b= 10;
+		int c= a+b;
+		System.out.println(c);
+		
+		*/
+		
+		
+	/*	int a = 7;
+		if(a%2==0)
+		{
+			System.out.println("number is an even");
+		}
+		else {
+			System.out.println("number is an odd");
+		}
+		*/ 
+		
+		
+		
+	/*	int a= 8;
+		int b=12;
+		if(a>b) {
+			System.out.println("8 is a largest");
+		}
+		if(a<b) {
+			System.out.println("12 ia a largest");
+		}
+		else {
+			System.out.println("none");
+		}
+		*/
+		
+	/*	Scanner scanner = new Scanner(System.in);       
+        System.out.print("Enter Principal Amount (P): ");
+        double P = scanner.nextDouble();
+        System.out.print("Enter Rate of Interest (R): ");
+        double R = scanner.nextDouble();
+        System.out.print("Enter Time in Years (T): ");
+        double T = scanner.nextDouble();
+        double SI = (P * R * T) / 100;
+        System.out.println("Simple Interest: " + SI);
+
+        scanner.close(); 
+        */
+		
+		
+	/*	int year= 2024;
+		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " is a Leap Year.");
+        } else {
+            System.out.println(year + " is NOT a Leap Year.");
+        }
+        */
+		
+		
+}
+
+}
