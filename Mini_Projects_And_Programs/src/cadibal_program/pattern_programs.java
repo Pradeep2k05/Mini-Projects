551948273c65270adf5e2daf1c8c356618468a54
java program <mudunjakandupudi006@gmail.com>
2025-07-10 20:34:17 +0530

    workout

--- Mini_Projects_And_Programs/src/cadibal_program/pattern_programs.java
+++ Mini_Projects_And_Programs/src/cadibal_program/pattern_programs.java
@@ -6,30 +6,37 @@ public class pattern_programs {
 
 	public static void main(String[] args) {
   
-		Scanner s=new Scanner(System.in);
-		 
-	//	System.out.print("Enter the value of a:");
-	//	int a =s.nextInt();
-		
-	//	int a=5;
-		
-		/*for(int i=1; i<=a; i++) {
-			for(int j=1; j<=i; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-			
-		}*/
-	
-		int a=5;
-		for(int i=1; i>=a; i++) {
-			for(int j=1; j<=i; j++) {
-				System.out.print("$");
-			}
-			System.out.println();
-			
+
+int a=5;
+	/*for(int i=0; i<=a; i++) {
+		for(int j=0; j<=i; j++) {
+			System.out.print("*");
 		}
+		System.out.println();
+	}*/
+
+
+	for(int i=1; i<=a; i++)
+	{
+		for(int j=i; j<a; j++) {
+			System.out.print(" ");
+		}
+		for(int j=1; j<=(2*i-1); j++) {
+			System.out.print("*");
+		}
+		System.out.println();
+		}
+	for(int i=a-1; i>=1; i--) {	for(int j=i; j<a; j++) {
+		System.out.print(" ");
+	}
+	for(int j=1; j<=(2*i-1); j++) {
+		System.out.print("*");
+	}
+	System.out.println();
+		
+	}
+	}
 	}
 	
 
-}
+
